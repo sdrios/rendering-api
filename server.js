4e1afb1883645acb952ad8368fd76caf45db5c21
Sarah <sarahd.rios@gmail.comm>
2019-11-26 20:33:28 -0600

    completed rendering api exercise

--- server.js
+++ server.js
@@ -19,34 +19,55 @@ var tweets = require('./abstractions/tweets.js');
 app.use(express.static(__dirname + '/public'));
 
 // GET /api/albums
-
+app.get('/api/albums', function(request, response){
+    response.send(albums);
+});
 
 // GET /api/circles
-
+app.get('/api/circles', function(request, response){
+    response.send(circles);
+});
 
 // GET /api/movies
-
+app.get('/api/movies', function(request, response){
+    response.send(movies);
+});
 
 // GET /api/nametags
-
+app.get('/api/nametags', function(request, response){
+    response.send(nameTags);
+});
 
 // GET /api/poker
-
+app.get('/api/poker', function(request, response){
+    response.send(poker);
+});
 
 // GET /api/rectangle
+app.get('/api/rectangle', function(request, response){
+    response.send(rectangle);
 
+});
 
 // GET /api/restaurants
-
+app.get('/api/restaurants', function(request, response){
+    response.send(restaurants);
+});
 
 // GET /api/students
-
+app.get('/api/students', function(request, response){
+    response.send(students);
+});
 
 // GET /api/surveys
-
+app.get('/api/surveys', function(request, response){
+    response.send(surveys);
+});
 
 // GET /api/tweets
-
+app.get('/api/tweets', function(request, response){
+    response.send(tweets);
+});
 
 app.listen(3000, function(){
     console.log('rendering-api is now listening on port 3000...');
